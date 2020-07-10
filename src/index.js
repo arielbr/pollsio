d2ba8ed67bac7fe2d38b06614ae7a457ac16e8e0
arielbr <55307744+arielbr@users.noreply.github.com>
2020-07-09 21:56:15 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,9 +7,8 @@ import Firebase, { FirebaseContext } from "./components/Firebase";
 
 ReactDOM.render(
   <React.StrictMode>
-    <FirebaseContext.Provider value={new Firebase()}>
+    
       <App />
-    </FirebaseContext.Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
